607655481bea41b7931848aa71d254af65a8b789
kripa Shankar Pandey <kripapandey302@gmail.com>
2023-02-23 21:21:18 +0530

    done

---
+++ sasta_bazar/src/Pages/SignleProductPage.jsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const SignleProductPage = ({ id }) => {
+  console.log(id, "id");
+  return <Box>hhhhhhhhhhhhh</Box>;
+};
+
+export default SignleProductPage;
