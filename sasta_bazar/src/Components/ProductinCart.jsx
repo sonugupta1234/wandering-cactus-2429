d4aaadb6bf0608a7a2f6b7872771dc0d1a51a270
kripa Shankar Pandey <kripapandey302@gmail.com>
2023-02-24 21:20:42 +0530

    done

---
+++ sasta_bazar/src/Components/ProductinCart.jsx
@@ -0,0 +1,90 @@
+import { DeleteIcon } from "@chakra-ui/icons";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const ProductinCart = ({
+  id,
+  title,
+  image,
+  price,
+  brand,
+  deletefunc,
+  handlecost,
+}) => {
+  const [quantity, setQuantity] = useState(1);
+  let sum = quantity * price;
+
+  const handledelete = () => {
+    return deletefunc(id);
+  };
+
+  useEffect(() => {
+    return handlecost(sum);
+  }, [quantity]);
+
+  return (
+    <div>
+      <Flex
+        justifyContent="space-evenly"
+        alignItems="center"
+        borderBottom="1px solid grey"
+        p="2rem"
+      >
+        <Box>
+          <img src={image} alt={title} width="25%" />
+        </Box>
+        <Box>
+          <Text as={"code"}>{title}</Text>
+        </Box>
+        <Box>
+          <Text as="i" fontSize="lg" color="green">
+            {price}
+          </Text>
+        </Box>
+        <Box color="black">
+          <Flex justifyContent="center" alignItems="center">
+            <button
+              style={{
+                color: "white",
+                backgroundColor: "red",
+                width: "50px",
+                height: "50px",
+                borderRadius: "10px",
+              }}
+              onClick={() => setQuantity((prev) => prev - 1)}
+              disabled={quantity == 0 ? true : false}
+            >
+              <Text mt="-15px" fontSize="5xl">
+                -
+              </Text>
+            </button>
+            <Text as="b" fontSize="xl" p={3}>
+              {quantity}
+            </Text>
+            <button
+              style={{
+                color: "white",
+                backgroundColor: "green",
+                width: "50px",
+                height: "50px",
+                borderRadius: "10px",
+              }}
+              onClick={() => setQuantity((prev) => prev + 1)}
+            >
+              <Text mt="-6px" fontSize="4xl">
+                +
+              </Text>
+            </button>
+          </Flex>
+        </Box>
+        <Box onClick={handledelete}>
+          <DeleteIcon boxSize={8} color="red" />
+        </Box>
+      </Flex>
+    </div>
+  );
+};
+
+export default ProductinCart;
