3b3b000dd1164115e2e2d6388025a2ffddd7fa91
uttamkrshaw <uttamkr5599@gmail.com>
2023-02-25 23:08:12 +0530

    admin pannel almost done

---
+++ sasta_bazar/src/Admin/Components/Product.jsx
@@ -0,0 +1,53 @@
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+} from '@chakra-ui/react'
+import React, { useEffect } from 'react'
+import { shallowEqual, useDispatch, useSelector } from "react-redux"
+import { GET_PRODUCT } from '../Redux/action';
+import Navbar from './Navbar/Navbar';
+import { ProductList } from './ProductList';
+export const Product = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(GET_PRODUCT)
+    }, [])
+    const { products, isLoading, isError } = useSelector((store) => { return { products: store.ProductReducer.products, isLoading: store.ProductReducer.isLoading, isError: store.ProductReducer.isError } }, shallowEqual)
+    console.log(products);
+    return (
+        <div>
+           <div>
+         <Navbar />
+           </div>
+           <div>
+                <TableContainer>
+                    <Table variant='striped' colorScheme='teal'>
+                        <TableCaption>Products List</TableCaption>
+                        <Thead>
+                            <Tr>
+                                <Th isNumeric>ID</Th>
+                                <Th>Image</Th>
+                                <Th>Title</Th>
+                                <Th>Brands</Th>
+                                <Th>Category</Th>
+                                <Th>Price</Th>
+                                <Th>Update</Th>
+                                <Th>Delete</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                    {products.map((el) => (< ProductList key={el.id} {...el} />))}
+                        </Tbody>
+                    </Table>
+                </TableContainer>
+                </div>
+        </div>
+    )
+}
---
+++ sasta_bazar/src/Admin/Components/ProductList.jsx
@@ -0,0 +1,28 @@
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+} from '@chakra-ui/react'
+import Delete from './Delete'
+import { Update } from './Update'
+
+export const ProductList = ({ id, image, price, title, brands, category }) => {
+    return (
+        <Tr>
+            <Td>{id}</Td>
+            <Td><img width={"30px"} src={image} alt="" /></Td>
+            <Td>{title}</Td>
+            <Td>{brands}</Td>
+            <Td>{category}</Td>
+            <Td isNumeric>{price}</Td>
+            <Td><Update id={id} price={price} title={title} brands={brands} category={category} image={image} /></Td>
+            <Td><Delete id={id} /></Td>
+        </Tr>
+    )
+}
