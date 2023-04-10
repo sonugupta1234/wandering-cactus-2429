5eb16a6040bb5d0c02ba813a07236829ad5e7885
kripa Shankar Pandey <kripapandey302@gmail.com>
2023-04-10 15:58:25 +0530

    Carousel width fixed

--- sasta_bazar/src/Components/Navbar.jsx
+++ sasta_bazar/src/Components/Navbar.jsx
@@ -24,20 +24,20 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-  Text
+  Text,
 } from "@chakra-ui/react";
 import React from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
-import logo from "../Images/logo.png"
-import user from "../Images/user.png"
+import logo from "../Images/logo.png";
+import user from "../Images/user.png";
 import { useSelector } from "react-redux";
 import { store } from "../Redux/store";
 
 function Navbar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const user = useSelector((store) => store.AuthReducer.user)
-  const isAuth = useSelector((store) => store.AuthReducer.isAuth)
+  const user = useSelector((store) => store.AuthReducer.user);
+  const isAuth = useSelector((store) => store.AuthReducer.isAuth);
   return (
     <>
       <Box px={4} bg="#f7fafc" position="sticky" top="0px" zIndex="100">
@@ -51,10 +51,7 @@ function Navbar() {
           />
           <HStack spacing={8} alignItems={"center"}>
             <Box>
-              <Image
-                src={logo}
-                alt="logo"
-              />
+              <Image src={logo} alt="logo" />
             </Box>
             <HStack
               as={"nav"}
@@ -73,8 +70,7 @@ function Navbar() {
             </HStack>
           </HStack>
           <Flex alignItems={"center"}>
-
-            {isAuth ?
+            {isAuth ? (
               <Menu>
                 <MenuButton
                   as={Button}
@@ -83,34 +79,30 @@ function Navbar() {
                   cursor={"pointer"}
                   minW={0}
                 >
-                  <Avatar
-                    size={"sm"}
-                    src={user}
-                  />
+                  <Avatar size={"sm"} src={user} />
                 </MenuButton>
                 <MenuList>
                   <MenuItem>{user.username}</MenuItem>
-                  <MenuItem >
-                    < BackdropExample />
+                  <MenuItem>
+                    <BackdropExample />
                   </MenuItem>
                   <MenuDivider />
                   <MenuItem>Logout</MenuItem>
                 </MenuList>
-              </Menu> :
+              </Menu>
+            ) : (
               <Link to="/login">
                 <Button
-                  as={'a'}
-                  display={{ base: 'none', md: 'inline-flex' }}
-                  fontSize={'sm'}
+                  as={"a"}
+                  display={{ base: "none", md: "inline-flex" }}
+                  fontSize={"md"}
                   fontWeight={600}
-                  color={'white'}
-                  bg="#00A699"
-                  href={'#'}
-                  _hover={"#00A699"}>
+                  colorScheme="facebook"
+                >
                   Sign In
                 </Button>
               </Link>
-            }
+            )}
           </Flex>
         </Flex>
 
@@ -135,9 +127,8 @@ function Navbar() {
 }
 export { Navbar };
 
-
 export const BackdropExample = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
@@ -146,95 +137,95 @@ export const BackdropExample = () => {
         isCentered
         onClose={onClose}
         isOpen={isOpen}
-        motionPreset='slideInBottom'
+        motionPreset="slideInBottom"
       >
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>User Details</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-           <UserDetails />
+            <UserDetails />
           </ModalBody>
           <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={onClose}>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
               Close
             </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
     </>
-  )
-}
-
+  );
+};
 
 export const UserDetails = () => {
-  const user = useSelector((store) => store.AuthReducer.user)
+  const user = useSelector((store) => store.AuthReducer.user);
   return (
     <Center py={6}>
       <Box
-        maxW={'270px'}
-        w={'full'}
-        bg={useColorModeValue('white', 'gray.800')}
-        boxShadow={'2xl'}
-        rounded={'md'}
-        overflow={'hidden'}>
+        maxW={"270px"}
+        w={"full"}
+        bg={useColorModeValue("white", "gray.800")}
+        boxShadow={"2xl"}
+        rounded={"md"}
+        overflow={"hidden"}
+      >
         <Image
-          h={'120px'}
-          w={'full'}
+          h={"120px"}
+          w={"full"}
           src={
-            'https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
+            "https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
           }
-          objectFit={'cover'}
+          objectFit={"cover"}
         />
-        <Flex justify={'center'} mt={-12}>
+        <Flex justify={"center"} mt={-12}>
           <Avatar
-            size={'xl'}
+            size={"xl"}
             src={user}
-            alt={'Author'}
+            alt={"Author"}
             css={{
-              border: '2px solid white',
+              border: "2px solid white",
             }}
           />
         </Flex>
 
         <Box>
-          <Stack spacing={0} align={'center'} mb={5}>
-            <Heading fontSize={'2xl'} fontWeight={500} fontFamily={'body'}>
+          <Stack spacing={0} align={"center"} mb={5}>
+            <Heading fontSize={"2xl"} fontWeight={500} fontFamily={"body"}>
               {user.username}
             </Heading>
-            <Text color={'gray.500'}>{user.email}</Text>
+            <Text color={"gray.500"}>{user.email}</Text>
           </Stack>
 
-          <Stack direction={'row'} justify={'center'} spacing={6}>
-            <Stack spacing={0} align={'center'}>
+          <Stack direction={"row"} justify={"center"} spacing={6}>
+            <Stack spacing={0} align={"center"}>
               <Text fontWeight={600}>{user.type}</Text>
-              <Text fontSize={'sm'} color={'gray.500'}>
+              <Text fontSize={"sm"} color={"gray.500"}>
                 Account Type
               </Text>
             </Stack>
-            <Stack spacing={0} align={'center'}>
+            <Stack spacing={0} align={"center"}>
               <Text fontWeight={600}>{user.order.length}</Text>
-              <Text fontSize={'sm'} color={'gray.500'}>
+              <Text fontSize={"sm"} color={"gray.500"}>
                 Items Ordered
               </Text>
             </Stack>
           </Stack>
 
           <Button
-            w={'full'}
+            w={"full"}
             mt={8}
-            bg={useColorModeValue('#151f21', 'gray.900')}
-            color={'white'}
-            rounded={'md'}
+            bg={useColorModeValue("#151f21", "gray.900")}
+            color={"white"}
+            rounded={"md"}
             _hover={{
-              transform: 'translateY(-2px)',
-              boxShadow: 'lg',
-            }}>
+              transform: "translateY(-2px)",
+              boxShadow: "lg",
+            }}
+          >
             Follow
           </Button>
         </Box>
       </Box>
     </Center>
-
-  )
-}
+  );
+};
